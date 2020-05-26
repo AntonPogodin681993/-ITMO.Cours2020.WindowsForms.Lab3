1daf9319b525d0cd18d1493b9c4059cbbafeef24
Anton Pogodin <anton.pogodin68@bk.ru>
2020-05-26 22:29:12 +0300

    Add project files.

---
+++ ITMO.Cours2020.WindowsForms.Lab3.Ex2/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ITMO.Cours2020.WindowsForms.Lab3.Ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControTimer21 = new ITMO.Cours2020.WindowsForms.Lab3.Ex2.UserControTimer2();
+            this.SuspendLayout();
+            // 
+            // userControTimer21
+            // 
+            this.userControTimer21.Location = new System.Drawing.Point(69, 51);
+            this.userControTimer21.Name = "userControTimer21";
+            this.userControTimer21.Size = new System.Drawing.Size(75, 23);
+            this.userControTimer21.TabIndex = 0;
+            this.userControTimer21.Text = "userControTimer21";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userControTimer21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControTimer2 userControTimer21;
+    }
+}
+
---
+++ ITMO.Cours2020.WindowsForms.Lab3.Ex2/UserControTimer2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Cours2020.WindowsForms.Lab3.Ex2
+{
+    public partial class UserControTimer2 : Control
+    {
+        public UserControTimer2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+            g.FillRectangle(Brushes.Blue, 0, 0, this.Width, this.Height);
+            pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font,
+                new SolidBrush(this.ForeColor), 0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
